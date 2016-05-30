4d2d977b49ec4272a4fb64c3c3e57e65487bfc60
Demian Hespe <demian.hespe@outlook.com>
2016-05-30 18:57:45 +0200

    Add executable to measure runtime of the sequential algorithm

---
+++ src/sequential_benchmark.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <argtable2.h>
+
+#include "timer.h"
+// #include "ils/ils.h"
+// #include "ils/local_search.h"
+#include "mis_log.h"
+#include "graph_io.h"
+// #include "reduction_evolution.h"
+#include "mis_config.h"
+// #include "greedy_mis.h"
+#include "parse_parameters.h"
+#include "data_structure/graph_access.h"
+// #include "data_structure/mis_permutation.h"
+#include "sequential/branch_and_reduce_algorithm.h"
+#include <memory>
+
+int main(int argn, char **argv) {
+    mis_log::instance()->restart_total_timer();
+    mis_log::instance()->print_title();
+    
+    MISConfig mis_config;
+    std::string graph_filepath;
+
+    // Parse the command line parameters;
+    int ret_code = parse_parameters(argn, argv, mis_config, graph_filepath);
+    if (ret_code) {
+        return 0;
+    }
+    mis_config.graph_filename = graph_filepath.substr(graph_filepath.find_last_of( '/' ) +1);
+    mis_log::instance()->set_config(mis_config);
+
+    // Read the graph
+    graph_access G;
+    graph_io::readGraphWeighted(G, graph_filepath);
+    mis_log::instance()->set_graph(G);
+    
+    // Print setup information
+    mis_log::instance()->print_graph();
+
+    // initialize full reducer
+    std::vector<std::vector<int>> adj(G.number_of_nodes());
+
+    // Build adjacency vectors
+    forall_nodes(G, node) {
+        adj[node].reserve(G.getNodeDegree(node));
+        forall_out_edges(G, edge, node) {
+            NodeID neighbor = G.getEdgeTarget(edge);
+            adj[node].push_back(neighbor);
+        } endfor
+    } endfor
+
+    std::unique_ptr<branch_and_reduce_algorithm> full_reducer = std::unique_ptr<branch_and_reduce_algorithm>(new branch_and_reduce_algorithm(adj, adj.size()));
+
+    double begin, end, elapsed_secs;
+    begin = omp_get_wtime();
+    full_reducer->reduce_graph_few_reductions();
+    end = omp_get_wtime();
+    elapsed_secs = double(end - begin);
+    std::cout << "Sequential took " << elapsed_secs << " seconds" << std::endl;
+
+    auto is_base = full_reducer->get_current_is_size_with_folds();
+    mis_log::instance()->print_reduction(mis_config, is_base, full_reducer->number_of_nodes_remaining());
+
+    return 0;
+}
