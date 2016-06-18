a6418d3de4e14f9f9211ecd97e706d0fc0341f80
Demian Hespe <demian.hespe@outlook.com>
2016-06-18 12:54:24 +0200

    Update benchmark script

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd fast-reductions/build
-graph_base_dir="../../../graphs/from_darren/"
-graph="petster-cat.graph"
-sizes=("1" "2")
+cd ../build
+graph_base_dir="/global_data/c_schulz/graph_collection/all_dimacs/dimacs10/archive/data/streets/"
+graph="europe.osm.graph"
+sizes=("1" "2" "4" "8" "16" "32" "48" "64")
 modes=("kahip" "parallel_kahip" "lpa")
 
 cmake .. -DNO_PREPROCESSING=OFF
@@ -15,7 +15,7 @@ do
 	for mode in "${modes[@]}"
 	do
 	   :
-		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode > ../../results/$graph-$size-$mode-preprocessing
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode-preprocessing
 	done
 done
 
@@ -27,6 +27,116 @@ do
 	for mode in "${modes[@]}"
 	do
 	   :
-		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode > ../../results/$graph-$size-$mode
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode
+	done
+done
+
+graph_base_dir="/global_data/c_schulz/graph_collection/rhgs_large/"
+graph="RHG-100000000-nodes-1000000000-edges.graph"
+
+cmake .. -DNO_PREPROCESSING=OFF
+make -j 4
+
+for size in "${sizes[@]}"
+do
+   :
+	for mode in "${modes[@]}"
+	do
+	   :
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode-preprocessing
+	done
+done
+
+cmake .. -DNO_PREPROCESSING=ON
+make -j 4
+for size in "${sizes[@]}"
+do
+   :
+	for mode in "${modes[@]}"
+	do
+	   :
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode
+	done
+done
+
+graph="RHG-100000000-nodes-2000000000-edges.graph"
+
+cmake .. -DNO_PREPROCESSING=OFF
+make -j 4
+
+for size in "${sizes[@]}"
+do
+   :
+	for mode in "${modes[@]}"
+	do
+	   :
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode-preprocessing
+	done
+done
+
+cmake .. -DNO_PREPROCESSING=ON
+make -j 4
+for size in "${sizes[@]}"
+do
+   :
+	for mode in "${modes[@]}"
+	do
+	   :
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode
+	done
+done
+
+graph_base_dir="/global_data/c_schulz/graph_collection/mis_huge/"
+graph="it-2004-sorted.graph"
+
+cmake .. -DNO_PREPROCESSING=OFF
+make -j 4
+
+for size in "${sizes[@]}"
+do
+   :
+	for mode in "${modes[@]}"
+	do
+	   :
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode-preprocessing
+	done
+done
+
+cmake .. -DNO_PREPROCESSING=ON
+make -j 4
+for size in "${sizes[@]}"
+do
+   :
+	for mode in "${modes[@]}"
+	do
+	   :
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode
+	done
+done
+
+graph="sk-2005-sorted.graph"
+
+cmake .. -DNO_PREPROCESSING=OFF
+make -j 4
+
+for size in "${sizes[@]}"
+do
+   :
+	for mode in "${modes[@]}"
+	do
+	   :
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode-preprocessing
+	done
+done
+
+cmake .. -DNO_PREPROCESSING=ON
+make -j 4
+for size in "${sizes[@]}"
+do
+   :
+	for mode in "${modes[@]}"
+	do
+	   :
+		./benchmark $graph_base_dir$graph --console_log --config social --num_partitions=$size --partitioner=$mode &> ../../results/$graph-$size-$mode
 	done
 done
