ca9e1e44de58d64998879758afc7cd413c646113
Demian Hespe <demian.hespe@outlook.com>
2016-08-14 23:03:30 +0200

    Add unconfined reduction to plotting script

--- scripts/draw_graphs.py
+++ scripts/draw_graphs.py
@@ -19,9 +19,6 @@ latexFileName = None
 if len(sys.argv) >= 3:
 	latexFileName = sys.argv[2]
 
-GraphData = namedtuple('GraphData', 'parallel_time parallel_size num_isolated_clique_reductions num_vertex_fold_reductions time_per_partition sequential_time')
-
-
 file = open(inputFile, "r")
 sizes = []
 parallel = True
@@ -33,6 +30,7 @@ num_isolated_clique_reductions = {}
 num_vertex_fold_reductions = {}
 num_twins_removed = {}
 num_twins_folded = {}
+num_unconfined_reductions = {}
 sequential_time = {}
 num_blocks = 0
 num_reps = 0
@@ -50,6 +48,7 @@ for line in file:
 				num_vertex_fold_reductions[num_blocks][block_num] /= num_reps
 				num_twins_removed[num_blocks][block_num] /= num_reps
 				num_twins_folded[num_blocks][block_num] /= num_reps
+				num_unconfined_reductions[num_blocks][block_num] /= num_reps
 			parallel_time[num_blocks] /= num_reps
 			parallel_size[num_blocks] /= num_reps
 			sequential_time[num_blocks] /= num_reps
@@ -70,6 +69,7 @@ for line in file:
 		num_vertex_fold_reductions[num_blocks] = [0] * num_blocks
 		num_twins_removed[num_blocks] = [0] * num_blocks
 		num_twins_folded[num_blocks] = [0] * num_blocks
+		num_unconfined_reductions[num_blocks] = [0] * num_blocks
 		parallel_time[num_blocks] = 0
 		parallel_size[num_blocks] = 0
 		sequential_time[num_blocks] = 0
@@ -102,6 +102,10 @@ for line in file:
 			block_num = int(words[0][:-1])
 			current_block_num_twins_folded = int(words[-1])
 			num_twins_folded[num_blocks][block_num] += current_block_num_twins_folded
+		if "Number of unconfined vertices removed" in line:
+			block_num = int(words[0][:-1])
+			current_block_num_unconfined_reductions = int(words[-1])
+			num_unconfined_reductions[num_blocks][block_num] += current_block_num_unconfined_reductions
 		if "Time spent applying reductions" in line:
 			block_num = int(words[0][:-1])
 			time_per_block[num_blocks][block_num] += float(words[-1])
@@ -123,40 +127,49 @@ sum_vertex_folds = [0] * len(sizes)
 sum_isolated_clique = [0] * len(sizes)
 sum_twins_removed = [0] * len(sizes)
 sum_twins_folded = [0] * len(sizes)
+sum_unconfined = [0] * len(sizes)
 for i in range(len(sizes)):
 	sum_vertex_folds[i] = sum(num_vertex_fold_reductions[sizes[i]])
 	sum_isolated_clique[i] = sum(num_isolated_clique_reductions[sizes[i]])
 	sum_twins_removed[i] = sum(num_twins_removed[sizes[i]])
 	sum_twins_folded[i] = sum(num_twins_folded[sizes[i]])
+	sum_unconfined[i] = sum(num_unconfined_reductions[sizes[i]])
 index = np.arange(len(sizes))
 width = 0.75
 plt.figure(1, figsize=(10, 10), dpi=500)
-plt.subplot(411)
+plt.subplot(511)
 plt.bar(index, sum_vertex_folds, width, color="red")
 plt.xlabel('# Partitions')
 plt.ylabel('# Reductions')
 plt.title('Number of vertex fold reductions performed in parallel')
 plt.xticks(index, sizes)
 plt.subplots_adjust(hspace = 0.7)
-plt.subplot(412)
+plt.subplot(512)
 plt.bar(index, sum_isolated_clique, width, color="red")
 plt.xlabel('# Partitions')
 plt.ylabel('# Reductions')
 plt.title('Number of isolated clique reductions performed in parallel')
 plt.xticks(index, sizes)
 plt.subplots_adjust(hspace = 0.7)
-plt.subplot(413)
+plt.subplot(513)
 plt.bar(index, sum_twins_removed, width, color="red")
 plt.xlabel('# Partitions')
 plt.ylabel('# Reductions')
 plt.title('Number of twin reductions (removed) performed in parallel')
 plt.xticks(index, sizes)
 plt.subplots_adjust(hspace = 0.7)
-plt.subplot(414)
+plt.subplot(514)
 plt.bar(index, sum_twins_folded, width, color="red")
 plt.xlabel('# Partitions')
 plt.ylabel('# Reductions')
 plt.title('Number of twin reductions (folded) performed in parallel')
+plt.xticks(index, sizes)
+plt.subplots_adjust(hspace = 0.7)
+plt.subplot(515)
+plt.bar(index, sum_unconfined, width, color="red")
+plt.xlabel('# Partitions')
+plt.ylabel('# Reductions')
+plt.title('Number of unconfined reductions performed in parallel')
 plt.xticks(index, sizes)
 plt.savefig(os.path.join(plotsDir, "num_reductions"))
 plt.clf()
@@ -211,11 +224,11 @@ if not latexFileName == None:
 	weight_string = os.path.basename(inputFile)
 	latexFile.write(graphName + " (" + numVerticesString + ", " + numEdgesString + ") " + weight_string.replace("_","\\_") + "\n")
 	latexFile.write("\\newline \n")
-	latexFile.write("\\begin{tabular}{l|rrrrrr|r}\n")
-	latexFile.write("number of blocks & parallel run time & parallel kernel size & vertex folds & isolated clique reductions & twin reductions (removed) & twin reductions (folded) & sequential run time \\\\ \n")
+	latexFile.write("\\begin{tabular}{l|rrrrrrr|r}\n")
+	latexFile.write("number of blocks & parallel run time & parallel kernel size & vertex folds & isolated clique reductions & twin reductions (removed) & twin reductions (folded) & unconfined reductions & sequential run time \\\\ \n")
 	latexFile.write("\\hline \n")
 	for i in range(len(sizes)):
-		latexFile.write(str(sizes[i]) + " & " + str(round(parallel_times[i], 1)) + " & " + str(int(parallel_sizes[i])) + " & " + str(int(sum_vertex_folds[i])) + " & " + str(int(sum_isolated_clique[i])) + " & " + str(int(sum_twins_removed[i])) + " & " + str(int(sum_twins_folded[i])) + " & " + str(round(sequential_times[i], 1)) + "\\\\ \n")
+		latexFile.write(str(sizes[i]) + " & " + str(round(parallel_times[i], 1)) + " & " + str(int(parallel_sizes[i])) + " & " + str(int(sum_vertex_folds[i])) + " & " + str(int(sum_isolated_clique[i])) + " & " + str(int(sum_twins_removed[i])) + " & " + str(int(sum_twins_folded[i])) + " & " + str(int(sum_unconfined[i])) + " & " + str(round(sequential_times[i], 1)) + "\\\\ \n")
 
 	latexFile.write("\\end{tabular} \n")
 	latexFile.write("\\vspace{1cm} \n\\newline \n")
