31c1b6af426e18b72ee89fb4ef318878a19a9865
Demian Hespe <demian.hespe@outlook.com>
2016-09-11 15:31:06 +0200

    Add lp reduction to plots and tables

--- scripts/draw_graphs.py
+++ scripts/draw_graphs.py
@@ -31,6 +31,7 @@ num_vertex_fold_reductions = {}
 num_twins_removed = {}
 num_twins_folded = {}
 num_unconfined_reductions = {}
+num_lp_removed = {}
 sequential_time = {}
 num_blocks = 0
 num_reps = 0
@@ -49,6 +50,7 @@ for line in file:
 				num_twins_removed[num_blocks][block_num] /= num_reps
 				num_twins_folded[num_blocks][block_num] /= num_reps
 				num_unconfined_reductions[num_blocks][block_num] /= num_reps
+			num_lp_removed[num_blocks] /= num_reps
 			parallel_time[num_blocks] /= num_reps
 			parallel_size[num_blocks] /= num_reps
 			sequential_time[num_blocks] /= num_reps
@@ -73,6 +75,7 @@ for line in file:
 		parallel_time[num_blocks] = 0
 		parallel_size[num_blocks] = 0
 		sequential_time[num_blocks] = 0
+		num_lp_removed[num_blocks] = 0
 	if "New repitition:" in line:
 		parallel = True
 		current_rep = int(words[-1])
@@ -106,6 +109,8 @@ for line in file:
 			block_num = int(words[0][:-1])
 			current_block_num_unconfined_reductions = int(words[-1])
 			num_unconfined_reductions[num_blocks][block_num] += current_block_num_unconfined_reductions
+		if "Number of vertices removed by LP reduction:" in line:
+			num_lp_removed[num_blocks] += int(words[-1])
 		if "Time spent applying reductions" in line:
 			block_num = int(words[0][:-1])
 			time_per_block[num_blocks][block_num] += float(words[-1])
@@ -123,53 +128,63 @@ print(sizes)
 print("Repititions:")
 print(num_reps)
 
+
 sum_vertex_folds = [0] * len(sizes)
 sum_isolated_clique = [0] * len(sizes)
 sum_twins_removed = [0] * len(sizes)
 sum_twins_folded = [0] * len(sizes)
 sum_unconfined = [0] * len(sizes)
+num_lp_sorted = [0] * len(sizes)
 for i in range(len(sizes)):
 	sum_vertex_folds[i] = sum(num_vertex_fold_reductions[sizes[i]])
 	sum_isolated_clique[i] = sum(num_isolated_clique_reductions[sizes[i]])
 	sum_twins_removed[i] = sum(num_twins_removed[sizes[i]])
 	sum_twins_folded[i] = sum(num_twins_folded[sizes[i]])
 	sum_unconfined[i] = sum(num_unconfined_reductions[sizes[i]])
+	num_lp_sorted[i] = num_lp_removed[sizes[i]]
 index = np.arange(len(sizes))
 width = 0.75
 plt.figure(1, figsize=(10, 10), dpi=500)
-plt.subplot(511)
+plt.subplot(611)
 plt.bar(index, sum_vertex_folds, width, color="red")
-plt.xlabel('# Partitions')
-plt.ylabel('# Reductions')
-plt.title('Number of vertex fold reductions performed in parallel')
+plt.xlabel('# Threads')
+plt.ylabel('# Vertices')
+plt.title('Number vertices removed by vertex fold reduction')
 plt.xticks(index, sizes)
 plt.subplots_adjust(hspace = 0.7)
-plt.subplot(512)
+plt.subplot(612)
 plt.bar(index, sum_isolated_clique, width, color="red")
-plt.xlabel('# Partitions')
-plt.ylabel('# Reductions')
-plt.title('Number of isolated clique reductions performed in parallel')
+plt.xlabel('# Threads')
+plt.ylabel('# Vertices')
+plt.title('Number of vertices removed isolated clique reduction')
 plt.xticks(index, sizes)
 plt.subplots_adjust(hspace = 0.7)
-plt.subplot(513)
+plt.subplot(613)
 plt.bar(index, sum_twins_removed, width, color="red")
-plt.xlabel('# Partitions')
-plt.ylabel('# Reductions')
-plt.title('Number of twin reductions (removed) performed in parallel')
+plt.xlabel('# Threads')
+plt.ylabel('# Vertices')
+plt.title('Number vertices removed of twin reduction (removed)')
 plt.xticks(index, sizes)
 plt.subplots_adjust(hspace = 0.7)
-plt.subplot(514)
+plt.subplot(614)
 plt.bar(index, sum_twins_folded, width, color="red")
-plt.xlabel('# Partitions')
-plt.ylabel('# Reductions')
-plt.title('Number of twin reductions (folded) performed in parallel')
+plt.xlabel('# Threads')
+plt.ylabel('# Vertices')
+plt.title('Number of vertices removed by twin reduction (folded)')
 plt.xticks(index, sizes)
 plt.subplots_adjust(hspace = 0.7)
-plt.subplot(515)
+plt.subplot(615)
 plt.bar(index, sum_unconfined, width, color="red")
-plt.xlabel('# Partitions')
-plt.ylabel('# Reductions')
-plt.title('Number of unconfined reductions performed in parallel')
+plt.xlabel('# Threads')
+plt.ylabel('# Vertices')
+plt.title('Number of vertices removed by unconfined reduction')
+plt.xticks(index, sizes)
+plt.subplots_adjust(hspace = 0.7)
+plt.subplot(616)
+plt.bar(index, num_lp_sorted, width, color="red")
+plt.xlabel('# Threads')
+plt.ylabel('# Vertices')
+plt.title('Number of vertices removed by lp reduction')
 plt.xticks(index, sizes)
 plt.savefig(os.path.join(plotsDir, "num_reductions"))
 plt.clf()
@@ -224,11 +239,11 @@ if not latexFileName == None:
 	weight_string = os.path.basename(inputFile)
 	latexFile.write(graphName + " (" + numVerticesString + ", " + numEdgesString + ") " + weight_string.replace("_","\\_") + "\n")
 	latexFile.write("\\newline \n")
-	latexFile.write("\\begin{tabular}{l|rrrrrrr|r}\n")
-	latexFile.write("number of blocks & parallel run time & parallel kernel size & vertex folds & isolated clique reductions & twin reductions (removed) & twin reductions (folded) & unconfined reductions & sequential run time \\\\ \n")
+	latexFile.write("\\begin{tabular}{l|rrrrrrrr|r}\n")
+	latexFile.write("number of blocks & parallel run time & parallel kernel size & vertex folds & isolated clique reductions & twin reductions (removed) & twin reductions (folded) & unconfined reductions & lp reductions & sequential run time \\\\ \n")
 	latexFile.write("\\hline \n")
 	for i in range(len(sizes)):
-		latexFile.write(str(sizes[i]) + " & " + str(round(parallel_times[i], 1)) + " & " + str(int(parallel_sizes[i])) + " & " + str(int(sum_vertex_folds[i])) + " & " + str(int(sum_isolated_clique[i])) + " & " + str(int(sum_twins_removed[i])) + " & " + str(int(sum_twins_folded[i])) + " & " + str(int(sum_unconfined[i])) + " & " + str(round(sequential_times[i], 1)) + "\\\\ \n")
+		latexFile.write(str(sizes[i]) + " & " + str(round(parallel_times[i], 1)) + " & " + str(int(parallel_sizes[i])) + " & " + str(int(sum_vertex_folds[i])) + " & " + str(int(sum_isolated_clique[i])) + " & " + str(int(sum_twins_removed[i])) + " & " + str(int(sum_twins_folded[i])) + " & " + str(int(sum_unconfined[i])) + " & " + str(int(num_lp_sorted[i])) + " & " + str(round(sequential_times[i], 1)) + "\\\\ \n")
 
 	latexFile.write("\\end{tabular} \n")
 	latexFile.write("\\vspace{1cm} \n\\newline \n")
